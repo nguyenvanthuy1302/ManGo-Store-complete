c7a93e52583b1d52396f20dc854b1415dc4bfa69
Phanvanbinh2000 <70782594+Phanvanbinh2000@users.noreply.github.com>
2022-12-05 01:23:54 +0700

    Message

--- Front-end/webpack.config.js
+++ Front-end/webpack.config.js
@@ -78,7 +78,7 @@ module.exports = {
     historyApiFallback: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000/',
+        target: 'https://mango-ms3e.onrender.com',
       },
     },
   },
